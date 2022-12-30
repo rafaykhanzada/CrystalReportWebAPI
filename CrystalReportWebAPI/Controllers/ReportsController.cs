bcf23bfbaa23b7dae71f358dc923bb02f44485d6
Abdul Rafay <rafaykhan3012@gmail.com>
2022-12-31 03:17:19 +0500

    Add project files.

---
+++ CrystalReportWebAPI/Controllers/ReportsController.cs
@@ -0,0 +1,67 @@
+﻿using CrystalReportWebAPI.Utilities;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CrystalReportWebAPI.Controllers
+{
+    [RoutePrefix("api/Reports")]
+    public class ReportsController : ApiController
+    {
+        [AllowAnonymous]
+        [Route("Financial/VarianceAnalysisReport")]
+        [HttpGet]
+        [ClientCacheWithEtag(60)]  //1 min client side caching
+        public HttpResponseMessage FinancialVarianceAnalysisReport()
+        {
+            string reportPath = "~/Reports";
+            string reportFileName = "CustomerLedgerReport.rpt";
+            string exportFilename = "CustomerLedgerReport.pdf";
+
+            HttpResponseMessage result = CrystalReport.RenderReport(reportPath, reportFileName, exportFilename);
+            return result;
+        }
+
+        [AllowAnonymous]
+        [Route("Demonstration/ComparativeIncomeStatement")]
+        [HttpGet]
+        [ClientCacheWithEtag(60)]  //1 min client side caching
+        public HttpResponseMessage DemonstrationComparativeIncomeStatement()
+        {
+            string reportPath = "~/Reports/Demonstration";
+            string reportFileName = "ComparativeIncomeStatement.rpt";
+            string exportFilename = "ComparativeIncomeStatement.pdf";
+
+            HttpResponseMessage result = CrystalReport.RenderReport(reportPath, reportFileName, exportFilename);
+            return result;
+        }
+
+        [AllowAnonymous]
+        [Route("VersatileandPrecise/Invoice")]
+        [HttpGet]
+        [ClientCacheWithEtag(60)]  //1 min client side caching
+        public HttpResponseMessage VersatileandPreciseInvoice()
+        {
+            string reportPath = "~/Reports/VersatileandPrecise";
+            string reportFileName = "Invoice.rpt";
+            string exportFilename = "Invoice.pdf";
+
+            HttpResponseMessage result = CrystalReport.RenderReport(reportPath, reportFileName, exportFilename);
+            return result;
+        }
+
+        [AllowAnonymous]
+        [Route("VersatileandPrecise/FortifyFinancialAllinOneRetirementSavings")]
+        [HttpGet]
+        [ClientCacheWithEtag(60)]  //1 min client side caching
+        public HttpResponseMessage VersatileandPreciseFortifyFinancialAllinOneRetirementSavings()
+        {
+            string reportPath = "~/Reports/VersatileandPrecise";
+            string reportFileName = "FortifyFinancialAllinOneRetirementSavings.rpt";
+            string exportFilename = "FortifyFinancialAllinOneRetirementSavings.pdf";
+
+            HttpResponseMessage result = CrystalReport.RenderReport(reportPath, reportFileName, exportFilename);
+
+            return result;
+        }
+    }
+}
---
+++ CrystalReportWebAPI/Utilities/CrystalReport.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using CrystalDecisions.CrystalReports.Engine;
+using Microsoft.Ajax.Utilities;
+
+namespace CrystalReportWebAPI.Utilities
+{
+    public static class CrystalReport
+    {
+        public static HttpResponseMessage RenderReport(string reportPath, string reportFileName, string exportFilename)
+        {
+			try
+			{
+                var rd = new ReportDocument();
+                
+                rd.Load(Path.Combine(System.Web.Hosting.HostingEnvironment.MapPath(reportPath), reportFileName));
+                rd.SetParameterValue(0, 3);
+                rd.SetParameterValue(1, "99428");
+                //rd.Load(@"C:\\Users\Abdul Rafay\\Documents\\Projects\\.NetCore\\APIs\\CrystalReportWebAPI\\CrystalReportWebAPI\\Reports\\CustomerLedgerReport.rpt");
+                MemoryStream ms = new MemoryStream();
+                rd.ExportToDisk(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat, Path.Combine(System.Web.Hosting.HostingEnvironment.MapPath(reportPath), reportFileName)+"4568.pdf");
+                using (var stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat))
+                {
+                    stream.CopyTo(ms);
+                }
+
+                var result = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new ByteArrayContent(ms.ToArray())
+                };
+                result.Content.Headers.ContentDisposition =
+                    new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment")
+                    {
+                        FileName = exportFilename
+                    };
+                result.Content.Headers.ContentType =
+                    new System.Net.Http.Headers.MediaTypeHeaderValue("application/pdf");
+                return result;
+            }
+			catch (System.Exception ex)
+			{
+
+				throw;
+			}
+        }
+    }
+}
